854defb87c859f7fe3d26b4f43a8f20fef4263cb
tdmahaff <151571455+tdmahaff@users.noreply.github.com>
2025-09-17 16:56:14 -0600

    Add files via upload

---
+++ 2_10.py
@@ -0,0 +1,8 @@
+# 2.10 Write a program that converts distances measured in kilometers to miles.
+# One kilometer is approximately 0.62 miles.
+
+def main():
+    kilometers = eval(input("Enter distance measured in kilometers: "))
+    print("Distance in miles is:", kilometers * 0.62)
+
+main()
